c94299c56bf279ad0b081505d6d0b1bb81dfb0a4
jessdjapar <jess.djapar@gmail.com>
2021-09-14 16:51:30 +0700

    req

--- todo/settings.py
+++ todo/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for todo project.
 
@@ -12,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'todo/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
